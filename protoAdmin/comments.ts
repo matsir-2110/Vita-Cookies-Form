74fbc989797d0f811ce8280ae6d1ce27092f61f6
colfredo32 <mortolasantino@gmail.com>
2026-05-22 23:15:14 -0300

    Prototipo de panel de administrador agregado

---
+++ protoAdmin/comments.ts
@@ -0,0 +1,16 @@
+import { pgTable, text, serial, timestamp } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod/v4";
+
+export const commentsTable = pgTable("comments", {
+  id: serial("id").primaryKey(),
+  texto: text("texto").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true }).notNull().defaultNow(),
+});
+
+export const insertCommentSchema = createInsertSchema(commentsTable).omit({
+  id: true,
+  createdAt: true,
+});
+export type InsertComment = z.infer<typeof insertCommentSchema>;
+export type Comment = typeof commentsTable.$inferSelect;
